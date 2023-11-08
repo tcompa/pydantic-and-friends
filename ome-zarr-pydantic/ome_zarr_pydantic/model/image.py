2bb9ef45aba4490950e88a0505ec32509d3b0d7b
Maximilian Schulz <maximilian.schulz@roche.com>
2023-11-08 12:17:11 +0100

    Optional fixes

--- ome-zarr-pydantic/ome_zarr_pydantic/model/image.py
+++ ome-zarr-pydantic/ome_zarr_pydantic/model/image.py
@@ -1,5 +1,5 @@
-from enum import Enum
-from gettext import translation
+# from enum import Enum
+from typing import Optional
 
 from pydantic import Field, model_validator, validator
 
@@ -8,16 +8,23 @@ from ome_zarr_pydantic.model.common import ConfigModel
 
 class Image(ConfigModel):
     multiscales: list["Multiscale"] = Field(min_length=1)
-    omero: "Omero" | None = None
+    omero: Optional["Omero"] = None
 
 
 class Multiscale(ConfigModel):
-    axes: list["Axe"] = Field(max_length=5)  # Note: will be relaxed in v0.5
+    """
+    Model for an element of `NgffImageMeta.multiscales`.
+
+    See https://ngff.openmicroscopy.org/0.4/#multiscale-md.
+    """
+    name: Optional[str] = None
     datasets: list["Dataset"] = Field(min_length=1)
+    version: Optional[str] = None
+    axes: list["Axis"] = Field(min_length=2, max_length=5)  # Note: will be relaxed in v0.5
 
     @validator("axes")
     @classmethod
-    def check_unique_axes(cls, value: list["Axe"]) -> list["Axe"]:
+    def check_unique_axes(cls, value: list["Axis"]) -> list["Axis"]:
         """Check that the axes are unique."""
         if len(value) != len(set(value)):
             raise ValueError("Axes must be unique.")
@@ -37,18 +44,28 @@ class Multiscale(ConfigModel):
         return self
 
 
-class Axe(ConfigModel):
+class Axis(ConfigModel):
+    """
+    Model for an element of `Multiscale.axes`.
+
+    See https://ngff.openmicroscopy.org/0.4/#axes-md.
+    """
     name: str  # The values MUST be unique across all "name" fields.
-    axe_type: str = Field(
+    axe_type: Optional[str] = Field(
         alias="type"
     )  # SHOULD be one of "space", "time" or "channel", but MAY take other values
-    unit: str | None = None
+    unit: Optional[str] = None
 
     def __hash__(self) -> int:
         return hash(self.name)
 
 
 class Dataset(ConfigModel):
+    """
+    Model for an element of `Multiscale.datasets`.
+
+    See https://ngff.openmicroscopy.org/0.4/#multiscale-md
+    """
     coordinate_transformations: list["CoordinateTransformation"] = Field(
         alias="coordinateTransformations",
         min_items=1,
@@ -57,8 +74,8 @@ class Dataset(ConfigModel):
 
 
 class CoordinateTransformation(ConfigModel):
-    scale: list[float] | None = None
-    translation: list[float] | None = None
+    scale: Optional[list[float]] = None
+    translation: Optional[list[float]] = None
     transformation_type: str = Field(
         alias="type",
     )
@@ -107,7 +124,7 @@ class Omero(ConfigModel):
     See https://ngff.openmicroscopy.org/0.4/#omero-md.
     """
     channels: list["Channel"]
-    rdefs: dict | None = None  # TODO: Add Type
+    rdefs: Optional[dict] = None  # TODO: Add Type
     version: str
 
 
@@ -118,10 +135,10 @@ class Channel(ConfigModel):
     See https://ngff.openmicroscopy.org/0.4/#omero-md.
     """
     window: "Window"
-    label: str | None = None
-    family: str | None = None
+    label: Optional[str] = None
+    family: Optional[str] = None
     color: str
-    active: bool | None = None
+    active: Optional[bool] = None
 
 
 class Window(ConfigModel):
@@ -133,8 +150,8 @@ class Window(ConfigModel):
     """
     max: float
     min: float
-    start: float | None = None
-    end: float | None = None
+    start: Optional[float] = None
+    end: Optional[float] = None
 
 
 # TODO: Check for Zarr Pydantic Models
